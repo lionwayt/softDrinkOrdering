3e827323779ddc8e893f42e22222ca3c024e8106
hiwot <hiwotmolalign2@gmail.com>
2022-12-03 18:44:20 +0300

    orderingApp

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  {id: '1', name: 'Jarritos', image: require('../assets/catergories/pizza.png')},
+  {id: '2', name: 'Mundet', image: require('../assets/catergories/burger.png')},
+  {id: '3', name: 'Sangria', image: require('../assets/catergories/sushi.png')},
+  {id: '4', name: 'Jenny', image: require('../assets/catergories/salad.png')},
+];
+
+export default categories;
---
+++ src/consts/foods.js
@@ -0,0 +1,32 @@
+const foods = [
+  {
+    id: '1',
+    name: 'Jarritos Mexican soda',
+    ingredients: 'Lemon',
+    price: '8.30',
+    image: require('../assets/jarritos2.jpg'),
+  },
+  {
+    id: '2',
+    name: 'Jarritos',
+    ingredients: 'Strawberry',
+    price: '7.10',
+    image: require('../assets/jarritos.jpg'),
+  },
+  {
+    id: '3',
+    name: 'Mundet',
+    ingredients: 'Apple',
+    price: '5.10',
+    image: require('../assets/mundet.jpg'),
+  },
+  {
+    id: '4',
+    name: 'Sangria Senorial',
+    ingredients: 'coffee',
+    price: '9.55',
+    image: require('../assets/sangria.jpg'),
+  },
+];
+
+export default foods;
